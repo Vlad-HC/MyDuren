7db763edae5d4b44843ce0121a2511effe9066dc
Vlad-HC <vladmul60@gmail.com>
2024-02-12 19:34:22 +0100

    test

--- test.py
+++ test.py
@@ -1,15 +1,43 @@
+from kivy.lang import Builder
+
+from kivymd.app import MDApp
+from kivymd.uix.behaviors import HoverBehavior
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+
+
 
 class Container(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # self.img1 = Image(source = 'images/card1.jpg')
+        self.btn = Button(text = 'bimbim')
 
-class MyFirstApp(App):
+
+
+
+class MyFirstApp(MDApp):
     def build(self):
-        BoxL = BoxLayout()
-        return Container()
-    
+    #     self.img1 = Image(source = 'images/card1.jpg')
+    #     self.img1.add_widget(HoverItem())
+    #     return self.img1
+        self.btn = Button(text = 'bimbim', size_hint = (0.5, 0.5))
+        self.btn.add_widget(HoverItem())
+        return self.btn
+
+class HoverItem(MyFirstApp, HoverBehavior):
+
+    def on_enter(self, *args):
+        self.btn.size_hint = (1.0, 1.0)
+        print('in')
+
+    def on_leave(self, *args):
+        self.btn.size = (2000,200)
+        print('out')
 
 if __name__ == '__main__':
     MyFirstApp().run()
+

539a7dbd9874353756696851d542e8d8ebdd37d7
Vlad-HC <vladmul60@gmail.com>
2024-05-20 15:30:48 +0200

    Update guitest .py

--- gui/guitest .py
+++ gui/guitest .py
@@ -1,28 +1,37 @@
 import pygame
+from modules.Deck import Deck
 pygame.init()
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
+
 screen = pygame.display.set_mode((600,800))
 clock = pygame.time.Clock()
 running = True
 spritesheet = pygame.image.load('images/clubs.png').convert_alpha()
 pygame.display.set_caption('Duren')
+
+CARD_WIDTH = 90
+CARD_HEIGHT = 128
+
 def get_image(sheet, width, height, scale, x,y):
     image = pygame.Surface((width,height)).convert_alpha()
     image.blit(sheet, (0,0),((x),(y), width, height))
     image = pygame.transform.scale(image, (width * scale, height * scale))
     return image
 
-card_1 = get_image(spritesheet, 90, 128, 1, 0, 0)
-card_2  = get_image(spritesheet, 90, 128, 1, 90, 0)
-card_3 = get_image(spritesheet, 90, 128, 1, 180, 0)
-card_4 = get_image(spritesheet, 90, 128, 1, 270, 0)
-card_5 = get_image(spritesheet, 90, 128, 1, 360, 0)
-card_6 = get_image(spritesheet, 90, 128, 1, 0, 128)
+card_1 = get_image(spritesheet, CARD_WIDTH, CARD_HEIGHT, 1, 0, 0)
+card_2  = get_image(spritesheet, CARD_WIDTH, CARD_HEIGHT, 1, 90, 0)
+card_3 = get_image(spritesheet, CARD_WIDTH, CARD_HEIGHT, 1, 180, 0)
+card_4 = get_image(spritesheet, CARD_WIDTH, CARD_HEIGHT, 1, 270, 0)
+card_5 = get_image(spritesheet, CARD_WIDTH, CARD_HEIGHT, 1, 360, 0)
+card_6 = get_image(spritesheet, CARD_WIDTH, CARD_HEIGHT, 1, 0, 128)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("red")
+    screen.fill("darkgreen")
     screen.blit(card_1,(0,0))
     screen.blit(card_2,(90,0))
     screen.blit(card_3,(180,0))
